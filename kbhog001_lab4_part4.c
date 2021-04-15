e5b56abff43102617be2a81330f66b5b7d896c22
Karan Bhogal <kbhog001@wch136-24.cs.ucr.edu>
2021-04-14 19:24:29 -0700

    Completed part 4

--- kbhog001_lab4_part4.c
+++ kbhog001_lab4_part4.c
@@ -1,122 +1,122 @@
-/*	Author: Karan Bhogal
- *  Partner(s) Name: 
- *	Lab Section: 21
- *	Assignment: Lab #4  Exercise #1
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
+*	Author: Karan Bhogal
+*	  *  Partner(s) Name: 
+*	    *	Lab Section: 21
+*	      *	Assignment: Lab #4  Exercise #3
+*	        *	Exercise Description: [optional - include for your own benefit]
+*	          *
+*	            *	I acknowledge all content contained herein, excluding template or example
+*	              *	code, is my own original work.
+*	                */
+ #include <avr/io.h>
+ #ifdef _SIMULATE_
+ #include "simAVRHeader.h"
+ #endif
 
-enum LA_States { Start, Wait, Check, S1, Lock } State;
+ enum LA_States { Start, Wait, Check, S1, Lock } State;
 
-void LED_latch()
-{
- switch(State) {
-   case Start:
+ void LED_latch()
+ {
+  switch(State) {
+    case Start:
 
-	if (PINA == 0x80){
-		State = Lock;
-	}
-	else if (PINA == 0x04) {
-		State = Wait;	
-	}
-	else {
-		State = Start;
-	}
-	break;
-  
-   case Lock:
-	State = Start;
-	break;
+ 	if (PINA == 0x80){
+ 		State = Lock;
+ 	}
+ 	else if (PINA == 0x04) {
+ 		State = Wait;	
+ 	}
+ 	else {
+ 		State = Start;
+ 	}
+ 	break;
 
-   case Wait:
-	if (PINA == 0x04){
-		State = Wait;
-	}
-	else if (PINA == 0x80){
-                State = Lock;
-        }
-	else if (PINA == 0x00){
-		State = Check;
-	}
-	else {
-		State = Start;
-	}
-	break;
+    case Lock:
+ 	State = Start;
+ 	break;
 
-   case Check:
-	if (PINA == 0x02){
-                State = S1;
-        }
-	else if (PINA == 0x80){
-                State = Lock;
-        }
-        else if (PINA == 0x00){
-                State = Check;
-        }
-	else {
-		State = Start;
-	}
-	break;
+    case Wait:
+ 	if (PINA == 0x04){
+ 		State = Wait;
+ 	}
+ 	else if (PINA == 0x80){
+                 State = Lock;
+         }
+ 	else if (PINA == 0x00){
+ 		State = Check;
+ 	}
+ 	else {
+ 		State = Start;
+ 	}
+ 	break;
 
-   case S1:
-	 if (PINA == 0x80){
-                State = Lock;
-        }
-	else {
-		State = Start;
-	}
-	break;
+    case Check:
+ 	if (PINA == 0x02){
+                 State = S1;
+         }
+ 	else if (PINA == 0x80){
+                 State = Lock;
+         }
+         else if (PINA == 0x00){
+                 State = Check;
+         }
+ 	else {
+ 		State = Start;
+ 	}
+ 	break;
 
-   default:
-	State = Start;
-	break;
-  } //transitions
+    case S1:
+ 	 if (PINA == 0x80){
+                 State = Lock;
+         }
+ 	else {
+ 		State = Start;
+ 	}	break;		
+ 	break;		
 
-  switch(State){
-   case Start:
-	break;
+    default:
+ 	State = Start;
+ 	break;
+   } //transitions
 
-   case Lock:
-	PORTB = 0x00;
-	break;
+   switch(State){
+    case Start:
+ 	break;
 
-   case Wait:
-	break;
+    case Lock:
+ 	PORTB = 0x00;
+ 	break;
 
-   case Check:
-	break;
-   
-   case S1:
-	if ( PORTB = 0x01 ) {
-		PORTB = 0x00;
-	}
-	else {
-		PORTB = 0x01;
-	}
-	break;
+    case Wait:
+ 	break;
 
-   default:
-	break;
-   }
-}
+    case Check:
+ 	break;
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-	
-	PORTB = 0x00;	
-	State = Start;
-    /* Insert your solution below */
+    case S1:
+ 	if ( PORTB == 0x01 ){
+ 		PORTB = 0x00;
+ 	}
+ 	else {
+ 		PORTB = 0x01;
+ 	}
+ 	break;
 
-    while (1) {
-	LED_latch();
+    default:
+ 	break;
     }
-    return 1;
-}
+ }
+
+ int main(void) {
+     /* Insert DDR and PORT initializations */
+ 	DDRA = 0x00; PORTA = 0xFF;
+ 	DDRB = 0xFF; PORTB = 0x00;
+
+ 	PORTB = 0x00;	
+ 	State = Start;
+     /* Insert your solution below */
+
+     while (1) {
+ 	LED_latch();
+     }
+     return 1;
+ }
